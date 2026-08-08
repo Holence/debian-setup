46c4372cd727a981dfbe4df1082a59548a0e577e
Holence <Holence08@gmail.com>
2026-08-08 13:37:32 +0800

    fast scroll shortcut for alacritty

--- alacritty/install.sh
+++ alacritty/install.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 echo "${COLOR_GREEN}install alacritty...${COLOR_END}"
+echo "${COLOR_GREEN}    (recommend using "cargo install alacritty" to install the latest version${COLOR_END}"
 sudo apt install alacritty
 
 mkdir -p ~/.config/alacritty/
